f409e7881d43a99fb039ea1beb4e500bcf8a9085
miaa9999 <toribo990228@gmail.com>
2024-05-02 16:25:04 +0900

    main controller

---
+++ src/main/java/com/example/shop_project_01/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.example.shop_project_01.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+       
+}
