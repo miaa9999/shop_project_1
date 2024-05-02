48d17a2f9ec81031d384b0062cbc23e29cdcfc52
miaa9999 <toribo990228@gmail.com>
2024-05-02 15:03:37 +0900

    0502 - start

---
+++ src/main/java/com/example/shop_project_01/ShopProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.shop_project_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProject01Application.class, args);
+	}
+
+}
