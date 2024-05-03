63144e4e9abb595bfb7f81bcf4ca06c5814ccfbc
miaa9999 <toribo990228@gmail.com>
2024-05-03 16:27:58 +0900

    data,properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into product(product_name,product_price,product_stock,main_category,sub_category,content)values("토끼 애착인형",12000,150,"인형","동물인형","아기들이 좋아할만한 토끼 애착인형");
+insert into product(product_name,product_price,product_stock,main_category,sub_category,content)values("강아지 애착인형",10000,120,"인형","동물인형","아기들이 좋아할만한 강아지 애착인형");
