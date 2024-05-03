3173f443b6bbfb4b89f4108cc869a699604c6770
seohyeon0221 <seohyeon010221@gmail.com>
2024-05-03 17:50:11 +0900

    #0503  서현 일단 만든것들

--- src/main/java/com/example/shop_project_01/controller/UserAccountController.java
+++ src/main/java/com/example/shop_project_01/controller/UserAccountController.java
@@ -1,8 +1,15 @@
 package com.example.shop_project_01.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("user")
 public class UserAccountController {
+    @GetMapping("/login")
+    public String login() {
+        return "/user/login";
+    }
 
 }
