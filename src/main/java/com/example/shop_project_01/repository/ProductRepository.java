9e6059196aaaaffdd5292b9b7c9a9832f6ba32bf
seohyeon0221 <seohyeon010221@gmail.com>
2024-05-07 12:00:53 +0900

    #0507 서현 12시까지

--- src/main/java/com/example/shop_project_01/repository/ProductRepository.java
+++ src/main/java/com/example/shop_project_01/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import com.example.shop_project_01.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
